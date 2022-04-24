033c1e409338380232c245cd2c3c9f1edb005b9d
Fettouhi Leila <101989161+FettouhiLeila@users.noreply.github.com>
2022-04-24 22:23:11 +0100

    Create database.js

---
+++ src/outils/database.js
@@ -0,0 +1,54 @@
+import {
+    writeFileSync
+} from 'fs'
+import Donneurs from '../../database/db.json'
+
+/**
+ * PODCASTS
+ */
+
+export function get_Donneurs() {
+    return Donneurs
+}
+
+export const get_Donneurs = id => {
+    const podcast = podcasts.find(podcast => Donneurs.id == id)
+
+    return podcast
+}
+
+
+export function add_podcast(podcast) {
+    let new_podcasts = [
+        ...podcasts,
+        {
+            ...podcast,
+            "id": Date.now().toString(36)
+        }
+    ]
+    const new_data = JSON.stringify(new_podcasts)
+
+    writeFileSync("database/podcasts.json", new_data)
+
+    return new_podcasts
+}
+
+    const new_data = JSON.stringify(podcasts)
+
+    writeFileSync("database/podcasts.json", new_data)
+
+    return podcasts
+}
+
+export function delete_Donneurs(id) {
+    let index = Donneurs.findIndex(Donneurs => Donneurs.id == id)
+
+    Donneurs.splice(index, 1)
+    delete_hosts(id)
+    const new_data = JSON.stringify(Donneurs)
+
+    writeFileSync("database/db.json", new_data)
+
+    return podcasts
+}
+
